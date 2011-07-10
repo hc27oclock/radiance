6b7d1ea0cb2340d7b2b9d5725647cba81cd0ab23
hc27oclock@gmail.com <hc27@localhost.localdomain>
2011-07-10 13:24:02 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radiance::Application.config.secret_token = 'f0c658ae7c76567218d680c1360a4d0bfb9a41abe8cd6a429ca581ce1adcee2186f4d8c82a9efa97a15ee95d59c3baf575002eb5990ac452b19f3030e30fbaaa'
